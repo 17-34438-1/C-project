eefc5dd018a19d8012a2420b98c1ffeb7641bf59
17-34438-1 <robin34438@gmail.com>
2021-07-28 18:29:29 +0600

    C Sharp Project

---
+++ BankMain/JointAccount.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BankMain
+{
+    partial class JointAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::BankMain.Properties.Resources.gd;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(759, 65);
+            this.panel1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::BankMain.Properties.Resources.icons8_arrow_pointing_left_64;
+            this.button1.Location = new System.Drawing.Point(11, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 43);
+            this.button1.TabIndex = 11;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(194, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(452, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "JOINT ACCOUNT DETAILS";
+            // 
+            // JointAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 464);
+            this.Controls.Add(this.panel1);
+            this.Name = "JointAccount";
+            this.Text = "JointAccount";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ BankMain/ManagerTransection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankMain
+{
+    public partial class ManagerTransection : Form
+    {
+        public ManagerTransection()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Loan_Manager_Palel lmp = new Loan_Manager_Palel();
+            this.Visible = false;
+            lmp.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Home h = new Home();
+            this.Visible = false;
+            h.Visible = true;
+        }
+    }
+}
